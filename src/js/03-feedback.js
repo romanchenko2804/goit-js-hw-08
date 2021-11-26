6614af9038595cde8401df8a3f421be044108247
romanchenko2804 <romanchenko2804@gmail.com>
2021-11-26 18:19:16 +0200

    third task - done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input[type="email"]'),
+}
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+savedForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function savedForm() {
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+    const parsedFormData = JSON.parse(savedFormData);
+
+     if (parsedFormData && parsedFormData.email) {
+      refs.input.value = parsedFormData.email;    
+    };
+
+    if (parsedFormData && parsedFormData.message) {
+        refs.textarea.value = parsedFormData.message;
+    };
+}
